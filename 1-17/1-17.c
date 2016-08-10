bbaeb1387d6516d6a0744ef8fa4c14e16c27bea2
David Alkire <david.alkire@gmail.com>
2016-08-10 06:50:44 -0400

    1-17

---
+++ 1-17/1-17.c
@@ -0,0 +1,43 @@
+/*
+ * Exercise 1-17.
+ * Write a program to print all the lines that are longer than 80
+ * characters.
+ */
+#include <stdio.h>
+
+#define MAXLINE 1000
+
+int _getline(char line[], int maxline);
+void copy(char to[], char from[]);
+
+int main()
+{
+	int len;
+	int max;
+	char line[MAXLINE];
+
+	max = 0;
+	while ((len = _getline(line, MAXLINE)) > 0) {
+		if (len > 80) {
+			printf("\n%d chars: %s\n", len, line);
+		}
+	}
+}
+
+int _getline(char s[], int lim)
+{
+	int c, i;
+
+	for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; i++) {
+		s[i] = c;
+	}
+
+	if (c == '\n') {
+		s[i] = c;
+		i++;
+	}
+
+	s[i] = '\0';
+
+	return i;
+}
